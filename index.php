68d305053486a65acc80cbb131e317a8ddc28f6d
juanxo13597 <juanxo97master@gmail.com>
2020-05-04 19:52:25 +0200

    base

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <script src="assets/js/main.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/jquery-3.5.0.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <title>Pizzeria Belsay</title>
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
