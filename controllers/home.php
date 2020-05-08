bbd459a4fd8aadfff6943879ae133dfd40485751
juanxo13597 <juanxo97master@gmail.com>
2020-05-08 15:47:46 +0200

    inicio de sesion con mensaje

--- controllers/home.php
+++ controllers/home.php
@@ -16,6 +16,13 @@ if(isset($_POST['inicio_sesion'])){
     }
 }
 
+if($inicio_sesion['estado'] == 'success'){
+    $message = "<div class='alert alert-success'>".$inicio_sesion['message']."</div>";
+}elseif($inicio_sesion['estado'] == 'error'){
+    $message = "<div class='alert alert-danger'>".$inicio_sesion['message']."</div>";
+}else{
+    $message = null;
+}
 
 require_once("views/home.php");
 ?>
