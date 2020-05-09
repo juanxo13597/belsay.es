437fceb9264c88d34ae11625622a24ff55592be3
juanxo13597 <juanxo97master@gmail.com>
2020-05-09 13:18:05 +0200

    diseño de administracion

---
+++ admin/inc/menu.php
@@ -0,0 +1,21 @@
+<!-- menu -->
+<div class="col-2 bg-light rounded">
+    <ul class="nav flex-column text-center">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Familias</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Carta</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Clientes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Horario</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link text-success" href="#">LOG</a>
+        </li>
+
+    </ul>
